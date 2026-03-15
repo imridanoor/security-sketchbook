f818e387389a1866425d073587682eddabe7faec
Professional <kidsunited@Zubairs-MacBook-Pro.local>
2026-03-15 17:10:31 -0400

    Add homepage illustration and layout

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,10 +1,13 @@
-import {defineConfig} from "sanity";
-import {structureTool} from "sanity/structure";
-import {schemaTypes} from "./src/sanity/schemaTypes";
+import { defineConfig } from "sanity";
+import { structureTool } from "sanity/structure";
+import { schemaTypes } from "./src/sanity/schemaTypes";
 
 export default defineConfig({
+  name: "default",
+  title: "SecuritySketchbook Studio",
   projectId: "3ouwxorl",
   dataset: "production",
+  basePath: "/studio",
   plugins: [structureTool()],
   schema: {
     types: schemaTypes,
